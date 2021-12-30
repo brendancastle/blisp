92aafa4835ae10f5bcc50b2f2a48461905ab538d
Brendan Castle <brendan.castle@icloud.com>
2021-12-29 19:18:58 -0500

    Implement basic grammar for polish notation arithmetic

--- parsing.c
+++ parsing.c
@@ -28,6 +28,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
+    // Create parsers
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Blisp     = mpc_new("blisp");
+
+    // Define parsers with the following language
+    mpca_lang(MPCA_LANG_DEFAULT,
+      "                                                         \
+        number      : /-?[0-9]+/ ;                              \
+        operator    : '+' | '-' | '*' | '/' ;                   \
+        expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+        blisp       : /^/ <operator> <expr>+ /$/ ;              \
+      ",
+      Number, Operator, Expr, Blisp);
+
     puts("Blisp Version 0.0.0.0.1");
     puts("Author: Brendan Castle\n");
     puts("Based on 'Build Your Own Lisp' by Daniel Holden\n");
@@ -38,9 +54,24 @@ int main(int argc, char** argv) {
         char* input = readline("blisp: ");
         add_history(input);
 
-        printf("You wrote: %s\n", input);
+        // Attempt to parse user input
+        mpc_result_t r;
+        if (mpc_parse("<stdin", input, Blisp, &r)) {
+            // On success print the AST
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else {
+            // Otherwise, print the error
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
     }
+
+    // Undefine and delete the parsers
+    mpc_cleanup(4, Number, Operator, Expr, Blisp);
 
     return 0;
 }
