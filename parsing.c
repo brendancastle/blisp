30c791ca4db23ae75b2d1c42d2c52648f022077b
Brendan Castle <brendan.castle@icloud.com>
2021-12-29 20:08:23 -0500

    Implement ^ operator

--- parsing.c
+++ parsing.c
@@ -33,6 +33,7 @@ long eval_op(long x, char* op, long y) {
     if (strcmp(op, "*") == 0) { return x * y; }
     if (strcmp(op, "/") == 0) { return x / y; }
     if (strcmp(op, "%") == 0) { return x % y; }
+    if (strcmp(op, "^") == 0) { return pow(x, y); }
 
     return 0;
 }
@@ -73,7 +74,7 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
       "                                                         \
         number      : /-?[0-9]+/ ;                              \
-        operator    : '+' | '-' | '*' | '/' | '%' ;             \
+        operator    : '+' | '-' | '*' | '/' | '%' | '^' ;       \
         expr        : <number> | '(' <operator> <expr>+ ')' ;   \
         blisp       : /^/ <operator> <expr>+ /$/ ;              \
       ",
