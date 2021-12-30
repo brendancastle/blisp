0cd13997bc947ef041eee0ae0f378cc81777cb11
Brendan Castle <brendan.castle@icloud.com>
2021-12-29 21:02:59 -0500

    Implement 'Lisp Value' structure and refactor for more robust error handling

--- parsing.c
+++ parsing.c
@@ -26,6 +26,63 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+// Enumeration of error types
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// Enumeration of lval types
+enum { LVAL_NUM, LVAL_ERR };
+
+// Define lval strcut
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+// Create a number type lval
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+// Create a error type lval
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.num = x;
+    return v;
+}
+
+// Print an lval
+void lval_print(lval v) {
+    switch (v.type) {
+        // If  this a number type lval, print the number
+        case LVAL_NUM:
+            printf("%li", v.num);
+            break;
+        case LVAL_ERR:
+            // Determine type of error and print it
+            if (v.err == LERR_DIV_ZERO) {
+                printf("Error: Division By Zero!");
+            }
+            if (v.err == LERR_BAD_OP) {
+                printf("Error: Invalid Operator!");
+            }
+            if (v.err == LERR_BAD_NUM) {
+                printf("Error: Invalid Number!");
+            }
+            break;
+    }
+}
+
+// Print an lval followed by a newline
+void lval_println(lval v) {
+    lval_print(v);
+    putchar('\n');
+}
+
 // Simple min function; returns the smallest number
 long min(long x, long y) {
     return (x < y ? x : y);
@@ -37,32 +94,48 @@ long max(long x, long y) {
 }
 
 // Determine proper operation based on operator string
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    if (strcmp(op, "%") == 0) { return x % y; }
-    if (strcmp(op, "^") == 0) { return pow(x, y); }
-    if (strcmp(op, "min") == 0) { return min(x, y); }
-    if (strcmp(op, "max") == 0) { return max(x, y); }
+lval eval_op(lval x, char* op, lval y) {
 
-    return 0;
+    // If either value is an error, return it
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    // Perform arithmetic
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+
+    // Check for division by zero
+    if (strcmp(op, "/") == 0) { 
+        return y.num == 0
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num); 
+    }
+
+    if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
+    if (strcmp(op, "^") == 0) { return lval_num(pow(x.num, y.num)); }
+
+    if (strcmp(op, "min") == 0) { return lval_num(min(x.num, y.num)); }
+    if (strcmp(op, "max") == 0) { return lval_num(max(x.num, y.num)); }
+
+    return lval_err(LERR_BAD_OP);
 }
 
 // Evaluate the given AST
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
 
-    // If tagged as number, return it directly
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+        // Check if there is some error in conversion
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     // The operator is always the second child
     char* op = t->children[1]->contents;
 
     // Store the third child in 'x'
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     // Iterate the remaining children
     int i = 3;
@@ -93,7 +166,7 @@ int main(int argc, char** argv) {
       ",
       Number, Operator, Expr, Blisp);
 
-    puts("Blisp Version 0.0.0.0.2");
+    puts("Blisp Version 0.0.0.0.4");
     puts("Author: Brendan Castle\n");
     puts("Based on 'Build Your Own Lisp' by Daniel Holden\n");
     puts("Press Ctrl+c to Exit\n");
@@ -106,8 +179,8 @@ int main(int argc, char** argv) {
         // Attempt to parse user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Blisp, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } 
         else {
